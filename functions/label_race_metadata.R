fadb9b98271a8549884d17b9ff9409ced5e80d66
Jacquie Tran <jacquietran@users.noreply.github.com>
2019-06-23 01:06:28 +1200

    Created functions to wrangle race metadata

---
+++ functions/label_race_metadata.R
@@ -0,0 +1,29 @@
+# Label the wrangled race metadata
+# and transpose to a one-row data frame
+label_race_metadata <- function(wrangled_metadata){
+  
+  library(dplyr)
+  
+  # Label the useful values with variable names
+  varnames <- c("event_num", rep(NA, 3), "boat_class_name", "race_date",
+                "boat_class_short", "race_type_short", "race_num",
+                "comp_details", rep(NA, 5), "comp_dates", rep(NA, 2))
+  labelled_metadata <- wrangled_metadata %>%
+    mutate(varnames = varnames) %>%
+    filter(!is.na(varnames))
+  
+  # Update varnames object with shorter list of variable names
+  varnames <- labelled_metadata$varnames
+  labelled_metadata <- labelled_metadata %>%
+    select(-varnames)
+  
+  # Transpose and apply labels as column names
+  labelled_metadata <- t(labelled_metadata)
+  colnames(labelled_metadata) <- varnames
+  rownames(labelled_metadata) <- NULL
+  
+  labelled_metadata <- data.frame(labelled_metadata)
+  
+  return(labelled_metadata)
+  
+}
---
+++ functions/wrangle_to_column.R
@@ -0,0 +1,23 @@
+# Wrangle the extracted race metadata to
+# a data frame of one column
+wrangle_to_column <- function(race_metadata){
+ 
+  library(dplyr)
+  
+  # Convert extracted race metadata to data frame
+  wrangled_metadata <- data.frame(race_metadata)
+  
+  # Wrangle the metadata into one column of values
+  column_values <- data.frame(rownames(wrangled_metadata))
+  colnames(column_values) <- "column_values"
+  
+  rownames(wrangled_metadata) <- NULL
+  
+  wrangled_metadata <- wrangled_metadata %>%
+    rename(column_values = race_metadata)
+  
+  wrangled_metadata <- rbind(wrangled_metadata, column_values)
+  
+  return(wrangled_metadata)
+   
+}
