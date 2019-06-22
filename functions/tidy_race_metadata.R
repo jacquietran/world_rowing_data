0e08f1062aa34fa8ae6deb68c48bac07417eba8c
Jacquie Tran <jacquietran@users.noreply.github.com>
2019-06-23 01:07:12 +1200

    Created a wrapper function that wrangles and cleans extracted race metadata

---
+++ functions/tidy_race_metadata.R
@@ -0,0 +1,22 @@
+# Tidy race metadata in one step,
+# combining wrangling and cleaning custom functions
+tidy_race_metadata <- function(race_metadata){
+  
+  # Wrangle
+  columnised_metadata <- wrangle_to_column(
+    race_metadata)
+  labelled_metadata <- label_race_metadata(
+    columnised_metadata)
+  
+  # Clean
+  cleaner_metadata <- clean_metadata_data_types(
+    labelled_metadata)
+  cleaner_with_comp_details <- clean_metadata_comp_details(
+    cleaner_metadata)
+  cleaned_with_comp_dates <- clean_metadata_comp_dates(
+    cleaner_with_comp_details
+  )
+    
+  return(cleaned_with_comp_dates)
+  
+}
