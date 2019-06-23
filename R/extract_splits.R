9b21364759bcd80ab4a2cecebeb93fbd0fca7fb7
Jacquie Tran <jacquietran@users.noreply.github.com>
2019-06-23 14:30:49 +1200

    Extended wrapper function testing to LW1x

--- R/extract_splits.R
+++ R/extract_splits.R
@@ -14,12 +14,22 @@ R.utils::sourceDirectory(here("functions"))
 
 # Extract tables from PDF ------------------------------------------------------
 
-a_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_A_Splits.pdf")
-b_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_B_Splits.pdf")
-c_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_C_Splits.pdf")
-d_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_D_Splits.pdf")
+a_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_A_Results.pdf")
+b_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_B_Results.pdf")
+c_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_C_Results.pdf")
+d_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_D_Results.pdf")
+
+lw1x_a_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_A_Results.pdf")
+lw1x_b_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_B_Results.pdf")
+lw1x_c_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_C_Results.pdf")
+lw1x_d_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_D_Results.pdf")
 
 tidy_a_final <- get_race_results_1x(a_final)
 tidy_b_final <- get_race_results_1x(b_final)
 tidy_c_final <- get_race_results_1x(c_final)
 tidy_d_final <- get_race_results_1x(d_final)
+
+tidy_lw1x_a_final <- get_race_results_1x(lw1x_a_final)
+tidy_lw1x_b_final <- get_race_results_1x(lw1x_b_final)
+tidy_lw1x_c_final <- get_race_results_1x(lw1x_c_final)
+tidy_lw1x_d_final <- get_race_results_1x(lw1x_d_final)
