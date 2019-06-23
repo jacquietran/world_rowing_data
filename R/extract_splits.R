c1e0673b8eaf048f22814cf062aa4c7bc02a5f4f
Jacquie Tran <jacquietran@users.noreply.github.com>
2019-06-23 19:04:49 +1200

    Extended testing of wrapper function to encompass finals and semi-finals

--- R/extract_splits.R
+++ R/extract_splits.R
@@ -6,6 +6,7 @@ library(here)
 library(tabulizer)
 library(dplyr)
 library(stringr)
+library(zoo)
 library(tidyr)
 
 # Call functions from source ---------------------------------------------------
@@ -14,15 +15,15 @@ R.utils::sourceDirectory(here("functions"))
 
 # Extract tables from PDF ------------------------------------------------------
 
-a_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_A_Results.pdf")
-b_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_B_Results.pdf")
-c_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_C_Results.pdf")
-d_final <- here("pdfs/20180914_2018_Worlds_LM1x_Final_D_Results.pdf")
+a_final <- here("pdfs/race_results/20180914_2018_Worlds_LM1x_Final_A_Results.pdf")
+b_final <- here("pdfs/race_results/20180914_2018_Worlds_LM1x_Final_B_Results.pdf")
+c_final <- here("pdfs/race_results/20180914_2018_Worlds_LM1x_Final_C_Results.pdf")
+d_final <- here("pdfs/race_results/20180914_2018_Worlds_LM1x_Final_D_Results.pdf")
 
-lw1x_a_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_A_Results.pdf")
-lw1x_b_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_B_Results.pdf")
-lw1x_c_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_C_Results.pdf")
-lw1x_d_final <- here("pdfs/20180914_2018_Worlds_LW1x_Final_D_Results.pdf")
+lw1x_a_final <- here("pdfs/race_results/20180914_2018_Worlds_LW1x_Final_A_Results.pdf")
+lw1x_b_final <- here("pdfs/race_results/20180914_2018_Worlds_LW1x_Final_B_Results.pdf")
+lw1x_c_final <- here("pdfs/race_results/20180914_2018_Worlds_LW1x_Final_C_Results.pdf")
+lw1x_d_final <- here("pdfs/race_results/20180914_2018_Worlds_LW1x_Final_D_Results.pdf")
 
 tidy_a_final <- get_race_results_1x(a_final)
 tidy_b_final <- get_race_results_1x(b_final)
@@ -33,3 +34,10 @@ tidy_lw1x_a_final <- get_race_results_1x(lw1x_a_final)
 tidy_lw1x_b_final <- get_race_results_1x(lw1x_b_final)
 tidy_lw1x_c_final <- get_race_results_1x(lw1x_c_final)
 tidy_lw1x_d_final <- get_race_results_1x(lw1x_d_final)
+
+# Adapting functions for use with semi-finals data -----------------------------
+
+semifinal_ab1 <- here("pdfs/race_results/20180913_2018_Worlds_LM1x_Semifinal_AB1_Results.pdf")
+semifinal_ab2 <- here("pdfs/race_results/20180913_2018_Worlds_LM1x_Semifinal_AB2_Results.pdf")
+
+race_results <- extract_race_results(semifinal_ab2)
